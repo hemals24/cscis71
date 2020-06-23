5923802498a18f09ae11e29c51bab17481f2a164
hemalsid@gmail.com <shemal@cloudhealthtech.com>
2020-06-23 10:54:26 -0400

    Hello World Application

---
+++ src/com/cscis71/Main.java
@@ -0,0 +1,10 @@
+package com.cscis71;
+
+public class Main {
+
+    public static void main(String[] args) {
+        if(args.length > 0) {
+            System.out.println("Hello, " + args[0] + "!");
+        }
+    }
+}
